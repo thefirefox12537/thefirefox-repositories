7dcd43e4fe0d3c57b6422e7fad9e14755840ac2b
The Firefox Foundation <reinmclaren33@gmail.com>
2022-03-16 04:35:18 +0700

    Update install-android-sdk-platform-tools.ps1

--- windows/install-android-sdk-platform-tools.ps1
+++ windows/install-android-sdk-platform-tools.ps1
@@ -68,9 +68,9 @@ if($IsAdmin -eq $false) {
 $availableExecutionPolicy = @("Unrestricted", "RemoteSigned", "ByPass")
 if((Get-ExecutionPolicy).ToString() -notin $availableExecutionPolicy) {
     Write-Host -BackgroundColor black -ForegroundColor red $(@(
-    "${ErrorAppInfo}: "
-    "PowerShell requires an execution policy in [$($availableExecutionPolicy -join ", ")] to run this script. "
-    "For example, to set the execution policy to 'RemoteSigned' please run PowerShell as Administrator and type : $($NewLine)"
+    "${ErrorAppInfo}:"
+    "PowerShell requires an execution policy in [$($availableExecutionPolicy -join ", ")] to run this script."
+    "For example, to set the execution policy to 'RemoteSigned' please run PowerShell as Administrator and type :", $NewLine
     "'Set-ExecutionPolicy RemoteSigned'"
     ) -join " ")
     if($Run_InvokeExpression) {pause}
